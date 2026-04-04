863978e45631bd5c4400cdaf7a20c77716b64c7e
bfbryan <ben@furnitureassist.com>
2026-04-04 09:48:12 -0400

    debug redirect

--- app/middleware.ts
+++ app/middleware.ts
@@ -4,6 +4,7 @@ import { NextResponse } from 'next/server'
 const DAWSON_USER_IDS = [
   'user_3BmTnGTVcPCuCJTpP8uKrQm4KXj', // Ben
   'user_3BodwTW4I7Vamt4t7wD3qeA7boM',  // Ray
+  'user_3BtKn01OMXSmi7eSsWvzvnEroCg',  //Dawson
 ]
 
 const isPublicRoute = createRouteMatcher([
@@ -19,13 +20,9 @@ export default clerkMiddleware(async (auth, req) => {
 
   // If signed in and hitting the root or sign-in page, redirect appropriately
   if (userId && (url.pathname === '/' || url.pathname === '/sign-in')) {
-    if (DAWSON_USER_IDS.includes(userId)) {
-      url.pathname = '/dawson'
-    } else {
-      url.pathname = '/dashboard'
-    }
+    url.pathname = '/redirect'
     return NextResponse.redirect(url)
-  }
+}
 
   // Protect non-public routes
   if (!isPublicRoute(req)) {
