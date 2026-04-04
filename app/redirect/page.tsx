d39115ff590d4fbdd2794a101aafa1a866bb1665
bfbryan <ben@furnitureassist.com>
2026-04-04 09:12:48 -0400

    add smart redirect page

---
+++ app/redirect/page.tsx
@@ -0,0 +1,18 @@
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+const DAWSON_USER_IDS = [
+  'user_3BmTnGTVcPCuCJTpP8uKrQm4KXj',
+  'user_3BodwTW4I7Vamt4t7wD3qeA7boM',
+]
+
+export default async function RedirectPage() {
+  const { userId } = await auth()
+  if (!userId) redirect('/sign-in')
+  
+  if (DAWSON_USER_IDS.includes(userId)) {
+    redirect('/dawson')
+  } else {
+    redirect('/dashboard')
+  }
+}
