bdee881380a86f634b865878a7b95c474392d389
bfbryan <ben@furnitureassist.com>
2026-04-04 09:52:40 -0400

    debug redirect

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ import { auth } from '@clerk/nextjs/server'
 
 export default async function Home() {
   const { userId } = await auth()
-  if (userId) redirect('/dashboard')
+  if (userId) redirect('/redirect')
   redirect('/sign-in')
 }
